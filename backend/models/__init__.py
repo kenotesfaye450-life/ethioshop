931a60270280fc22298fb2a5f3fea73676066bee
Keno Tesfaye <kenotesfaye450@gmail.com>
2026-05-29 08:31:55 +0300

    Update __init__.py

--- backend/models/__init__.py
+++ backend/models/__init__.py
@@ -3,21 +3,21 @@ EthioShop Models Package
 Imports all model classes for easy access throughout the application.
 """
 
-from backend.models.user import User
-from backend.models.product import Product
-from backend.models.product_image import ProductImage
-from backend.models.order import Order, OrderItem
-from backend.models.admin import Admin
-from backend.models.refund import Refund
-from backend.models.request import Request
-from backend.models.referral import Referral
-from backend.models.credit_transaction import CreditTransaction
-from backend.models.telegram_user import TelegramUser
-from backend.models.site_setting import SiteSetting
-from backend.models.order_message import OrderMessage
-from backend.models.payment import Payment
-from backend.models.review import Review
-from backend.models.bot_cart import BotCart
+from models.user import User
+from models.product import Product
+from models.product_image import ProductImage
+from models.order import Order, OrderItem
+from models.admin import Admin
+from models.refund import Refund
+from models.request import Request
+from models.referral import Referral
+from models.credit_transaction import CreditTransaction
+from models.telegram_user import TelegramUser
+from models.site_setting import SiteSetting
+from models.order_message import OrderMessage
+from models.payment import Payment
+from models.review import Review
+from models.bot_cart import BotCart
 
 __all__ = [
     'User',
